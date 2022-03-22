0621ce7274c00f4937a07a233b55baee18d1b47d
JakeSimoes <60676244+JakeSimoes@users.noreply.github.com>
2022-03-22 15:29:40 -0400

    Update
    Added
    - Arm Controls
    - Movement Controls
    - Grip Controls
    - Overall Improvements

--- main.py
+++ main.py
@@ -12,9 +12,56 @@ from threading import Thread
 
 """This file handles all the Nao interactions"""
 
-"""ROBOT IP"""
-ip = "127.0.0.1"
+ip = raw_input("Enter ROBOT IP or leave blank for localhost.\n")
+speed = float(raw_input("Enter speed 0.0-1.0 (Stick to 0.1 for real robots)\n"))
+if ip.isspace():
+    ip = '127.0.0.1'
 port = 9559
+
+
+def armThread():
+    # Wait a good amount of time for variables to get initialized
+    while True:
+        if 'rPitch' not in globals():
+            time.sleep(1)
+        else:
+            break
+    global rPitch, rRoll, rYaw, reRoll, lPitch, lRoll, lYaw, leRoll, rTrig, lTrig, rRot, lRot, motionProxy
+    while True:
+        motionProxy.setAngles("RShoulderPitch", -rPitch,
+                              speed)
+        motionProxy.setAngles("RShoulderRoll", rRoll,
+                              speed)
+        motionProxy.setAngles("RElbowYaw", rYaw,
+                              speed)
+        motionProxy.setAngles("RElbowRoll", reRoll,
+                              speed)
+        motionProxy.setAngles("LShoulderPitch", -lPitch,
+                              speed)
+        motionProxy.setAngles("LShoulderRoll", lRoll,
+                              speed)
+        motionProxy.setAngles("LElbowYaw", lYaw,
+                              speed)
+        motionProxy.setAngles("LElbowRoll", leRoll,
+                              speed)
+        motionProxy.setAngles("RHand", rTrig, 0.1)
+        motionProxy.setAngles("LHand", lTrig, 0.1)
+        if rRot > .60:
+            rRot = 1.7
+        elif rRot < -.60:
+            rRot = -1.7
+        else:
+            rRot = rRot * 2
+
+        if lRot > .60:
+            lRot = 1.7
+        elif lRot < -.60:
+            lRot = -1.7
+        else:
+            lRot = lRot * 2
+
+        motionProxy.setAngles("RWristYaw", rRot, speed)
+        motionProxy.setAngles("LWristYaw", lRot, speed)
 
 
 def visionThread():
@@ -41,11 +88,11 @@ def visionThread():
 
     while True:
         # get image
-        result = videoDevice.getImageRemote(captureDevice);
+        result = videoDevice.getImageRemote(captureDevice)
         if result == None:
-            print('cannot capture.')
+            print('Failed to get image from robot, try restarting the simulation or robot.')
         elif result[6] == None:
-            print('no image data string.')
+            print('Failed to receive valid data from the robot, try restarting the simulation or robot.')
         else:
 
             # translate value to mat
@@ -62,38 +109,42 @@ def visionThread():
             socket.send(pickle.dumps(image))
 
 
-# this file is the main file which controls NAO
-angle1 = 1
-angle2 = 1
+motionProxy = ALProxy("ALMotion", ip, port)
+postureProxy = ALProxy("ALRobotPosture", ip, 9559)
+postureProxy.goToPosture("StandInit", 0.5)
+motionProxy.setStiffnesses('Head', 1.0)
+# starts both threads for controlling vision and arm movement
 thread = Thread(target=visionThread)  # makes a thread for the imageThread function
 thread.start()
-tts = ALProxy("ALTextToSpeech", ip, port)
-motionProxy = ALProxy("ALMotion", ip, port)
-motionProxy.setStiffnesses('Head', 1.0)
-process = subprocess.Popen("""C:/Python38/python.exe vr.py""",
-                           stdout=subprocess.PIPE)
+thread2 = Thread(target=armThread)  # makes a thread for the armThread function
+thread2.start()
 
+print("Starting VR process...")
+"""OpenVR doesn't work with python 2.7, to move around that issue
+   a python 3.8 install with it is used and communicated to via sockets."""
+
+process = subprocess.Popen("vr/vr.exe")
+print("Success!")
+print("Waiting for VR boot up...")
+# connecting to the VR data socket
+context2 = zmq.Context()
+socket2 = context2.socket(zmq.REQ)
+socket2.connect("tcp://localhost:5556")
+print("Successfully connected to VR process!")
+print("Robot control is live, be cautious to not over stress your robot.\n"
+      "Make sure to close the application before taking off the headset.")
 while True:
-    # TODO: Socket the headset connection?
-    for line in iter(process.stdout.readline,
-                     ''):  # replace '' with b'' for Python 3
-        if "Headset:  " in line and "[I]" not in line:
-            headline = line
-            sys.stdout.write(line)
-            coords = headline.replace("Headset:  ", "").replace("[", "").replace("]", "").replace(
-                "  ", " ").replace("1  ", "").strip().split(" ")
-            try:
-                angle2 = math.radians(-1.0 * float(coords[0]) - 10)
-            except:
-                pass
-            try:
-                angle1 = math.radians(float(coords[1]))
-            except:
-                pass
-            fractionMaxSpeed = 0.3
-            # TODO: Fix wrapping yaw values before -80 and after 80
-            motionProxy.setAngles("HeadYaw", angle1,
-                                  fractionMaxSpeed)
-            motionProxy.setAngles("HeadPitch", angle2,
-                                  fractionMaxSpeed)
-
+    socket2.send(" ")
+    message = socket2.recv_string()
+    yaw, pitch, lPitch, lRoll, lYaw, leRoll, rPitch, rRoll, \
+    rYaw, reRoll, lTrig, rTrig, lY, lX, rY, rX, lRot, rRot = map(float, message.split(" "))
+    motionProxy.setAngles("HeadYaw", math.radians(yaw),
+                          0.3)
+    motionProxy.setAngles("HeadPitch", math.radians(pitch),
+                          0.3)
+    if lY > 0.1 or lX > 0.1 or lY < -0.1 or lX < -0.1:
+        motionProxy.setWalkTargetVelocity(lY, -lX, 0.0, 0.0)
+    elif rY > 0.1 or rX > 0.1 or rY < -0.1 or rX < -0.1:
+        motionProxy.moveToward(rY, -rX, 0.0, [["Frequency", 0.5]])
+    else:
+        motionProxy.setWalkTargetVelocity(0.0, 0.0, 0.0, 0.0)
--- vr.py
+++ vr.py
@@ -1,3 +1,5 @@
+from klampt import WorldModel
+from klampt.model import ik
 import math
 import time
 import zmq
@@ -9,7 +11,6 @@ from PIL import ImageOps
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from threading import Thread
-from scipy.spatial.transform import Rotation as R
 
 
 """This file handles all of the VR data and interactions with the headset"""
@@ -19,9 +20,8 @@ def read_texture(image_data):
     # Set the dimensions of the image
     width = 320
     height = 240
-    # Receive the image and flip it vertically
+    # Receive the image and clean it up
     img = Image.fromarray(image_data).convert('RGBA')
-    # img = Image.open('robot.png').convert('RGBA')
     img = ImageOps.flip(img)
     data = numpy.array(list(img.getdata()), numpy.int8)
     # After texture_id is defined it is
@@ -40,25 +40,70 @@ def read_texture(image_data):
 
 
 # Convert the 3x4 position/rotation matrix to a x,y,z location and the appropriate Euler angles (in radians)
-def convert_to_radians(pose_mat):
+def convert_to_cartesian(pose_mat):
     x = pose_mat[0][3]
     y = pose_mat[1][3]
     z = pose_mat[2][3]
-
-    # Algorithm 1
-
-    # Read here: https://steamcommunity.com/app/358720/discussions/0/343787920117426152/
-    pitch = math.atan2(pose_mat[2][1], pose_mat[2][2])
-    yaw = math.asin(pose_mat[2][0])
-    roll = math.atan2(-pose_mat[1][0], pose_mat[0][0])
-    return [x, y, z, yaw, pitch, roll]
+    return [x, y, z]
 
 
-def draw_sphere():
+def convert_to_radians(pose_mat):
+    global center
+    r_x = math.sqrt(max(0, 1 + pose_mat[0][0] - pose_mat[1][1] - pose_mat[2][2])) / 2
+    r_y = math.sqrt(max(0, 1 - pose_mat[0][0] + pose_mat[1][1] - pose_mat[2][2])) / 2
+    r_z = math.sqrt(max(0, 1 - pose_mat[0][0] - pose_mat[1][1] + pose_mat[2][2])) / 2
+    r_x *= math.copysign(1, r_x * (-pose_mat[2][1] + pose_mat[1][2]))
+    r_y *= math.copysign(1, r_y * (-pose_mat[0][2] + pose_mat[2][0]))
+    r_z *= math.copysign(1, r_z * (pose_mat[1][0] - pose_mat[0][1]))
+
+    result = ((r_y * 180) + 180)
+
+    if center > 180:
+        upperbound = center - 180
+    else:
+        upperbound = 180 + center
+
+    if center < result < upperbound:
+        return [-min(abs(result - center), 360 - abs(result - center)), (r_x*40)]
+    else:
+        return [min(abs(result - center), 360 - abs(result - center)), (r_x*40)]
+
+
+def convert_to_radians_controller(pose_mat):
+    r_z = math.sqrt(max(0, 1 - pose_mat[0][0] - pose_mat[1][1] + pose_mat[2][2])) / 2
+    r_z *= math.copysign(1, r_z * (pose_mat[1][0] - pose_mat[0][1]))
+    return r_z
+
+
+def center_headset(pose_mat):
+    global center
+    r_y = math.sqrt(max(0, 1 - pose_mat[0][0] + pose_mat[1][1] - pose_mat[2][2])) / 2
+    r_y *= math.copysign(1, r_y * (-pose_mat[0][2] + pose_mat[2][0]))
+    center = ((r_y * 180) + 180)
+
+
+def ik_solve(xyz, arm):
+    ratios = [0.2690286461961894, -0.5225724060631975, -0.5510363369906585]
+    relative_robot = []
+    for index, i in enumerate(HMDtoRobot):
+        relative_robot.append(xyz[i] * ratios[index])
+    # Set them as an objective and solve
+    # Iterations are set low so it can be fast, may be weird at times
+    if arm == 'right':
+        obj = ik.objective(right_link, local=[0, 0, 0], world=relative_robot)
+        ik.solve_global(obj, iters=100, tol=1e-3, numRestarts=50, activeDofs=[65, 66, 67, 68, 69])
+        return robot.getConfig()[65:69]
+    else:
+        obj = ik.objective(left_link, local=[0, 0, 0], world=relative_robot)
+        ik.solve_global(obj, iters=100, tol=1e-3, numRestarts=50, activeDofs=[35, 36, 37, 38, 39])
+        return robot.getConfig()[35:39]
+
+
+def draw():
     return
 
 
-def overlayrefresh():
+def overlay_refresh():
     # Connecting to the main.py image socket
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
@@ -68,8 +113,7 @@ def overlayrefresh():
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
     glutCreateWindow("")
     glutHideWindow()
-    # A blank function is used as there is no use for the window
-    glutDisplayFunc(draw_sphere)
+    glutDisplayFunc(draw)  # A blank function is used as there is no use for the window
     # Setting up a memory buffer for textures
     fb = glGenFramebuffers(1)
     glBindFramebuffer(GL_FRAMEBUFFER, fb)
@@ -77,19 +121,21 @@ def overlayrefresh():
     glBindRenderbuffer(GL_RENDERBUFFER, depth_buffer)
     glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 240, 320)
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, depth_buffer)
-    # time.sleep(0.5)
+    time.sleep(0.1)
     while True:
-        socket.send(b"Hello")
+        pass
+        socket.send(b" ")
 
         #  Get the reply.
-        message = socket.recv()
-        result = pickle.loads(message, encoding='latin1')
+        reply = socket.recv()
+        result = pickle.loads(reply, encoding='latin1')  # Decoding latin1 for python 2.7 data conversion
         openvr.VROverlay().clearOverlayTexture(handle)  # Clears the last image
         texture = openvr.Texture_t()  # Create a texture object
         while True:
             try:  # The read_texture func is called in a try statement to catch memory access errors
                 texture.handle = read_texture(result)  # Setting the handle attribute to the result of read_texture()
             except Exception as e:
+                print(e)
                 continue
             finally:
                 break
@@ -97,57 +143,104 @@ def overlayrefresh():
         texture.eColorSpace = openvr.ColorSpace_Gamma  # Setting the ColorSpace to Gamma as it doesn't crash
         while True:
             try:
-                openvr.VROverlay().setOverlayTexture(handle, texture)  # Finally sending the texture to SteamVR
-                openvr.VROverlay().showOverlay(handle)  # Making Sure to show it!
+                openvr.VROverlay().setOverlayTexture(handle, texture)  # Finally sending the texture to OpenVR
+                openvr.VROverlay().showOverlay(handle)  # Making sure to show it!
             except Exception as e:
+                print('error! 2')
                 continue
             finally:
                 break
 
 
-
-
+context2 = zmq.Context()
+socket2 = context2.socket(zmq.REP)
+socket2.bind("tcp://*:5556")
+center = 0
 poses = []  # Will be populated with proper type after first call
 # The next four lines create the Overlay and set its distance
 openvr.init(openvr.VRApplication_Scene)
 openvr.init(openvr.VRApplication_Overlay)
+system = openvr.IVRSystem()
 handle = openvr.VROverlay().createOverlay("foo", "bar")  # creates the overlay
 openvr.VROverlay().setOverlayWidthInMeters(handle, 1.0)  # sets overlay size
+
+poses, _ = openvr.VRCompositor().waitGetPoses(poses, None)
+hmd_pose = poses[openvr.k_unTrackedDeviceIndex_Hmd]
+# the following lines convert the input to a numpy array, splice it to get
+# only the data we need and then converts that euler to x,y,z
+# arr = numpy.array(list(hmd_pose.mDeviceToAbsoluteTracking))
+
 arr = openvr.HmdMatrix34_t  # a reference to the data type
 # creates an array structure that will be put into a HmdMatrix34 variable.
-bar = arr((1.0, 0, 0, 0), (0, 1.0, 0, -0.05), (0, 0, 1.0, -0.5))
+bar = arr((.5, 0, 0, 0), (0, .5, 0, -0.05), (0, 0, .5, -0.5))
 test = openvr.Structure.__new__(openvr.HmdMatrix34_t)  # creating a empty var
 test._setArray(bar)  # setting the empty HmdMatrix34 var to the bar array.
 # sets the overlays position to be relative to the headset
 openvr.VROverlay().setOverlayTransformTrackedDeviceRelative(handle,
                                                             openvr.k_unTrackedDeviceIndex_Hmd,
                                                             test)
-thread = Thread(target=overlayrefresh)  # makes a thread for the imageThread function
-thread.start()  # starts the imageThread which will update the video feed.
+# now setting up the IK solver...
+world = WorldModel()
+world.loadFile("nao_rob/nao.rob")
+robot = world.robot(0)
+left_link = robot.link(39)
+right_link = robot.link(69)
+HMDtoRobot = [2, 0, 1]  # Defining the indexes to relate HMD coords to the robots
+rArmRotations = [0, 0, 0, 0]
+lArmRotations = [0, 0, 0, 0]
+thread = Thread(target=overlay_refresh)  # Makes a thread for the imageThread function
+thread.start()  # Starts the imageThread which will update the video feed.
 while True:
+    # Grabbing VR data
+    _, left_controller_state = system.getControllerState(
+        system.getTrackedDeviceIndexForControllerRole(openvr.TrackedControllerRole_LeftHand))
+    _, right_controller_state = system.getControllerState(
+        system.getTrackedDeviceIndexForControllerRole(openvr.TrackedControllerRole_RightHand))
     poses, _ = openvr.VRCompositor().waitGetPoses(poses, None)
     hmd_pose = poses[openvr.k_unTrackedDeviceIndex_Hmd]
-    # the following lines convert the input to a numpy array, splice it to get
-    # only the data we need and then converts that euler to x,y,z
-    arr = numpy.array(list(hmd_pose.mDeviceToAbsoluteTracking))
-    r = R.from_matrix(arr[0:, 0:-1])
-    print("\nHeadset: ", r.as_euler('xyz', degrees=True))
+    # This grabs that state of the controller
+    lc_pose = poses[openvr.k_EButton_IndexController_B]
+    rc_pose = poses[openvr.k_EButton_IndexController_A]
+    lc_rotation = convert_to_radians_controller(lc_pose.mDeviceToAbsoluteTracking)
+    rc_rotation = convert_to_radians_controller(rc_pose.mDeviceToAbsoluteTracking)
+    HMD_rotation = convert_to_radians(list(hmd_pose.mDeviceToAbsoluteTracking))
+    # Grabbing positional data and formatting it
+    lc_position = convert_to_cartesian(list(lc_pose.mDeviceToAbsoluteTracking))
+    rc_position = convert_to_cartesian(list(rc_pose.mDeviceToAbsoluteTracking))
+    HMD_position = convert_to_cartesian(list(hmd_pose.mDeviceToAbsoluteTracking))
 
-    # poses, _ = openvr.VRCompositor().waitGetPoses(poses, None)
-    # lc_pose = poses[openvr.k_EButton_IndexController_B]
-    # arr = numpy.array(list(lc_pose.mDeviceToAbsoluteTracking))
-    # agh = convert_to_radians(arr)
-    #
-    # try:
-    #     print("\nController 1: ", agh)
-    # except:
-    #     pass
-    # poses, _ = openvr.VRCompositor().waitGetPoses(poses, None)
-    # rc_pose = poses[openvr.k_EButton_IndexController_A]
-    # arr = numpy.array(list(lc_pose.mDeviceToAbsoluteTracking))
-    # try:
-    #     print("Controller 2: ", convert_to_euler(arr))
-    # except:
-    #     pass
+    rc_rob_position = [0, 0, 0]
+    # The position of the controller relative to the HMD is used
+    # to determine if it's past left and negative or below, also negative
+    for index, i in enumerate(HMD_position):
+        if i > rc_position[index]:
+            rc_rob_position[index] = -(abs(i - rc_position[index]))
+        else:
+            rc_rob_position[index] = abs(rc_position[index] - i)
+    rc_rob_position[2] = -rc_rob_position[2]
 
-openvr.shutdown()
+    lc_rob_position = [0, 0, 0]
+    # The position of the controller relative to the HMD is used
+    # to determine if it's past left and negative or below, also negative
+    for index, i in enumerate(HMD_position):
+        if i > lc_position[index]:
+            lc_rob_position[index] = -(abs(i - lc_position[index]))
+        else:
+            lc_rob_position[index] = abs(lc_position[index] - i)
+    lc_rob_position[2] = -lc_rob_position[2]
+
+    # Triggering the head rotation with side triggers.
+    if bool(right_controller_state.ulButtonPressed >> 2 & 1):
+        center_headset(list(hmd_pose.mDeviceToAbsoluteTracking))
+    rc_trigger = [right_controller_state.rAxis[1].x]
+    lc_trigger = [left_controller_state.rAxis[1].x]
+    rc_stick = [right_controller_state.rAxis[0].y, right_controller_state.rAxis[0].x]
+    lc_stick = [left_controller_state.rAxis[0].y, left_controller_state.rAxis[0].x]
+    # TODO: Have ratios be calculated for the users specific arms
+    # Scale the coordinates
+
+    final_packet = HMD_rotation + ik_solve(lc_rob_position, 'left') + \
+        ik_solve(rc_rob_position, 'right') + lc_trigger + rc_trigger + lc_stick + rc_stick + \
+                   [lc_rotation] + [rc_rotation]
+    message = socket2.recv()
+    socket2.send_string("{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}".format(*final_packet))
